52d0811c00ae01eccb3a1323193fdd0ce4b3791c
Benson Mwangi <kimaniben@gmail.com>
2023-12-14 12:26:16 -0500

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
    <div id = "footer"> 
       <p>&copy; 2023 ibuka - A knowledge repository </p>
-      <p><span>Developers</span> Ussein,Mark,Benson</p>
+      <p><span>Developers :</span> Ussein,Mark,Benson</p>
     </div>
   );
 };
